3ff9e9f8b9ac4ade97d0d1fd462df0d746a20bf6
Saige Rutherford <saruther@med.umich.edu>
2019-05-24 23:27:18 -0400

    nonaggr

--- AROMA/AROMA.sh
+++ AROMA/AROMA.sh
@@ -4,4 +4,4 @@ echo ${1}
 DIR=`dirname ${1}`
 FILE=`basename ${1}`
 
-/usr/bin/python2.7 /home/slab/users/mangstad/repos/ICA-AROMA/ICA_AROMA.py -in ${DIR}/${FILE} -out ${DIR}/ICA_AROMA -mc ${DIR}/rp*.txt -m /net/parasite/SchizConnect/ROIs/rs_spm12_mni_icbm152_nlin_sym_09b_1xero_s8_thrp35_mask.nii -tr 2 -den both > $DIR/aroma.log
+/usr/bin/python2.7 /home/slab/users/mangstad/repos/ICA-AROMA/ICA_AROMA.py -in ${DIR}/${FILE} -out ${DIR}/ICA_AROMA -mc ${DIR}/rp*.txt -m /net/parasite/SchizConnect/ROIs/rs_spm12_mni_icbm152_nlin_sym_09b_1xero_s8_thrp35_mask.nii -tr 2 -den nonaggr > $DIR/aroma.log
